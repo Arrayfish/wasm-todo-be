b967a8de099b888376b885f67ab31539629a1a86
Array-fish <uekusa_hideaki@yahoo.co.jp>
2023-11-30 19:39:32 +0900

    fix: mod.rsを削除　type 途中

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use migration::{Migrator, MigratorTrait};
-
+use sea_orm::{Database, EntityTrait, FromQueryResult, IntoActiveModel, QueryFilter, QueryOrder, Set, DatabaseConnection};
+use entity::{prelude::*, *};
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -11,12 +11,11 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-
-async fn get_all_todolists_and_todos() -> impl Responder {
+async fn get_all_todolists_and_todos(db: web::Data<DatabaseConnection>) -> impl Responder {
     // get user id from the session
-    
+    let todo_lists: Vec<todo_list::Model> = TodoList::all(db.get_ref()).await.unwrap();
     // access to the database
-    HttpResponse::Ok().body("Hey there!")
+    HttpResponse::Ok().body(todo_lists)
 }
 
 async fn create_todo(req_body: String) -> impl Responder {
@@ -33,17 +32,18 @@ async fn delete_todo() -> impl Responder {
     // access to the database
     HttpResponse::Ok().body("Hey there!")
 }
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let connection = sea_orm::Database::connect(&database_url).await?;
-    Migrator::up(&connection, None).await?;
+    let database_url = "postgres://postgres:password@localhost:5436/todo_db";
+    let db = Database::connect(database_url).await.unwrap();
     HttpServer::new(|| {
         App::new()
+        .app_data(web::Data::new(db))
             .service(
                 web::scope("/todo")
                     .route("/todos", web::get().to(get_todos))
             )
+            .route("/", web::get().to(get_all_todolists_and_todos))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
