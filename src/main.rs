b2ee269c6831b68048636dd78ed1a31437bd913a
Array-fish <uekusa_hideaki@yahoo.co.jp>
2023-12-04 19:53:43 +0900

    feat: todo_list entityについてもcreate, update, deleteを追加

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result};
 use sea_orm::{Database, EntityTrait,  DatabaseConnection, ActiveModelTrait, ActiveValue};
 use entity::{prelude::*, *};
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 
 #[derive(Debug, Clone)]
 pub struct AppState {
@@ -84,6 +84,54 @@ async fn delete_todo(data: web::Data<AppState>, todo: web::Json<todo::Model>) ->
         Err(_) => HttpResponse::InternalServerError().body("Error delete todo!"),
     }
 }
+
+async fn create_todo_list(data: web::Data<AppState>, todo_list: web::Json<todo_list::Model>) -> impl Responder {
+    // access to the database
+    let db = &data.db;
+    let mut todo_list: todo_list::ActiveModel = todo_list.into_inner().into();
+    todo_list.id = ActiveValue::NotSet;
+    let res = todo_list.save(db).await;
+    match res{
+        Ok(_) => HttpResponse::Ok().body(format!("Todo list created! id: {:?}",res.unwrap().id.unwrap())),
+        Err(err) => {
+            println!("Error: {}",err);
+            HttpResponse::InternalServerError().body("Error creating todo list!")
+        }
+    }
+}
+
+async fn update_todo_list(data: web::Data<AppState>, todo_list: web::Json<todo_list::Model>) -> impl Responder {
+    // access to the database
+    let db = &data.db;
+    let todo_list: todo_list::ActiveModel = todo_list.into_inner().into();
+    println!("Todo list: {:?}", &todo_list);
+    let todo_list = todo_list.reset_all();
+    let res = todo_list.update(db).await;
+    match res{
+        Ok(res) => {
+            println!("Todo list updated: {:?}",res);
+            HttpResponse::Ok().body("Todo list updated!")
+        }
+        Err(err) => {
+            println!("Error: {}",err);
+            HttpResponse::InternalServerError().body("Error update todo list!")
+        }
+    }
+}
+
+async fn delete_todo_list(data: web::Data<AppState>, todo_list: web::Json<todo_list::Model>) -> impl Responder {
+    // access to the database
+    let db = &data.db;
+    let res = TodoList::delete_by_id(todo_list.id).exec(db).await;
+    match res {
+        Ok(_) => HttpResponse::Ok().body("Todo list deleted!"),
+        Err(err) => {
+            let error_message = format!("Error deleting todo list: {}", err);
+            HttpResponse::InternalServerError().body(error_message)
+        },
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let database_url = "postgres://postgres:password@localhost:5436/todo_db";
@@ -93,9 +141,18 @@ async fn main() -> std::io::Result<()> {
         App::new()
         .app_data(web::Data::new(app_state.clone()))
             .route("/", web::get().to(get_all_todolists_and_todos))
-            .route("/create", web::post().to(create_todo))
-            .route("/update", web::post().to(update_todo))
-            .route("/delete", web::post().to(delete_todo))
+            .service(
+                web::scope("/todo")
+                .route("/create", web::post().to(create_todo))
+                .route("/update", web::post().to(update_todo))
+                .route("/delete", web::post().to(delete_todo))
+            )
+            .service(
+                web::scope("/todo_list")
+                .route("/create", web::post().to(create_todo_list))
+                .route("/update", web::post().to(update_todo_list))
+                .route("/delete", web::post().to(delete_todo_list)
+                ))
     })
     .bind(("127.0.0.1", 8081))?
     .run()
